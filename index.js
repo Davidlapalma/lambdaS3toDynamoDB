56848fcda2889ffc6d5d7235b01812e9b48c152a
Ruben Vazquez <rubenvg@outlook.com>
2018-04-26 16:46:45 +0100

    PutItem on DynamoDB

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const aws = require('aws-sdk')
 const s3 = new aws.S3();
+var documentClient = new aws.DynamoDB.DocumentClient();
 
 exports.handler = function(event, context, exit){
     console.log('Se llamó a Lambda s3toDynamoDB')
     // Get the object from the event and show its content type
     const bucket = event.Records[0].s3.bucket.name;
-    const fileName = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+    const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
     const params = {
        Bucket: bucket,
-       Key: fileName,
+       Key: key,
     };
     s3.getObject(params, function(err, data){
          if (err) {
            console.log('ERROR ' + err);
            exit(err);
          } else {
-           console.log('Se ha recibido el archivo', fileName, JSON.stringify(data));
+           console.log('Se ha recibido el archivo', key, JSON.stringify(data));
           // data.Body.data devuelve un Buffer
-          console.log((new Buffer(120)).toString('hex'))
          }
      }); 
-     return 'Anything';
+     var params2 = {
+TableName: "Usuarios",
+Item: {
+    "Id_Fichero":"x",
+    "dni":"453"
+}
+
+};
+console.log('Llamando Put item')
+documentClient.put(params2, function(err, data) {
+  if (err) console.log(err);
+  else console.log(data);
+});
+     return 'hola';
 };
